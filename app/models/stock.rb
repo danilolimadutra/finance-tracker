9f1065915648efdff446c3ccac554f6a52070749
Danilo Lima Dutra <danilolimadutra@gmail.com>
2018-03-13 22:16:13 -0300

    adicionado restrição na adição de stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,6 +6,10 @@ class Stock < ApplicationRecord
     where(ticker: ticker_symbol).first
   end
 
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
   def self.new_from_lookup(ticker_symbol)
     begin
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
