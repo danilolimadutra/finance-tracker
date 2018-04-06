1d77eb9ca6cb65e49e5bb0339c39ab26524acdf4
Danilo Lima Dutra <danilolimadutra@gmail.com>
2018-04-05 21:51:02 -0300

    corrigido resultado da busca vazia por usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,18 @@ class UsersController < ApplicationController
 
   def search
     @users = User.search(params[:search_param])
-    render json: @users 
+    render json: @users
   end
+
+  def search
+   if params[:search_param].blank?
+     flash.now[:danger] = "You have entered an empty search string"
+   else
+     @users = User.search(params[:search_param])
+     flash.now[:danger] = "No users match this search criteria" if @users.blank?
+   end
+   respond_to do |format|
+     format.js { render partial: 'friends/result' }
+   end
+ end
 end
